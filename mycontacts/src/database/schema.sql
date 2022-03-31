3af488db3a35e977d390e126e14ea2bf9f86b24e
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2022-03-30 22:06:50 -0300

    Database schema - mycontacts

---
+++ mycontacts/src/database/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS mycontacts;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS categories (
+  id  UUID NOT NULL UNIQUE DEFAULT  uuid_generate_v4(),
+  name VARCHAR NOT NULL
+);
