a14608a1c1d6f72bea6497b57a1635e7898ff3d6
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2022-06-04 22:43:53 -0300

    Update App.js
    -Rendereizando listas

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -2,6 +2,12 @@ import React from "react";
 import Post from "./Post";
 import Header from "./Header";
 
+const posts = [
+  { title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
+  { title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
+  { title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
+  { title: 'Title#04', subtitle: 'Sub#04', likes: 60 },
+];
 
 function App() {
   return (
@@ -12,27 +18,16 @@ function App() {
 
       <hr />
 
-      <Post
-        likes={20}
-        post={{
-          title: "Título da notícia 01",
-          subtitle: "Subtítulo da notícia"
-        }}
-      />
-      <Post
-        likes={10}
-        post={{
-          title:"Título da notícia 02",
-          subtitle:"Subtítulo da notícia"
-        }}
-      />
-      <Post
-        likes={50}
-        post={{
-          title:"Título da notícia 03",
-          subtitle:"Subtítulo da notícia"
-        }}
-      />
+      {posts.map( post => (
+        <Post
+        key={post.title}
+          likes={post.likes}
+          post={{
+            title: post.title,
+            subtitle: post.subtitle,
+          }}
+        />
+      ))}
 
     </>
 
