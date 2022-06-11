72f7ce87de984c4b9a59cb7f4c1aeb11a1acc290
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2022-06-11 17:30:56 -0300

    Renderização condicional

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
-import Post from "./Post";
-import Header from "./Header";
+import React, { useState } from 'react';
+import Post from './Post';
+import Header from './Header';
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
-    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
-    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
-    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 60 },
+    {
+      id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false,
+    },
+    {
+      id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true,
+    },
+    {
+      id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false,
+    },
+    {
+      id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 60, read: false,
+    },
   ]);
-
-  console.log({ posts });
 
   function handleRefresh() {
     setPosts((prevState) => [
@@ -20,40 +26,36 @@ function App() {
         title: `Title#0${prevState.length + 1}`,
         subtitle: `Sub#0${prevState.length + 1}`,
         likes: 50,
-      }
+      },
     ]);
   }
 
-  function handleRemovePost(postId){
+  function handleRemovePost(postId) {
     console.log({ postId });
     setPosts((prevState) => prevState.filter((post) => post.id !== postId));
   }
 
   return (
     <>
-      <Header >
+      <Header>
         <h2>Posts da semana</h2>
         <button onClick={handleRefresh}>Atualizar</button>
       </Header>
 
       <hr />
 
-      {posts.map(post => (
+      {posts.map((post) => (
         <Post
-          key={post.title}
+          key={post.id}
           likes={post.likes}
           onRemove={handleRemovePost}
-          post={{
-            id: post.id,
-            title: post.title,
-            subtitle: post.subtitle,
-          }}
+          post={post}
         />
       ))}
 
     </>
 
-  )
+  );
 }
 
 export default App;
--- react-fundamentos/src/Post.js
+++ react-fundamentos/src/Post.js
@@ -1,17 +1,33 @@
-import React from "react";
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/destructuring-assignment */
+import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Post(props) {
+  if (props.post.read) {
+    return (
+      <h2>
+        {props.post.title}
+        {' '}
+        já foi lido.
+      </h2>
+    );
+  }
 
   return (
     <>
       <article>
-        <strong>{props.post.title}</strong>
+        <strong>
+          {props.post.read ? <s>{props.post.title}</s> : props.post.title}
+          ;
+        </strong>
         <button onClick={() => props.onRemove(props.post.id)}>Remover</button>
         <br />
         <small>{props.post.subtitle}</small>
         <br />
-        Media: {props.likes / 2}
+        Media:
+        {' '}
+        {props.post.likes / 2}
       </article>
       <br />
     </>
@@ -19,12 +35,13 @@ export default function Post(props) {
 }
 
 Post.propTypes = {
-  likes: PropTypes.number.isRequired,
   onRemove: PropTypes.func.isRequired,
   post: PropTypes.shape({
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
+    likes: PropTypes.number.isRequired,
+    read: PropTypes.bool.isRequired,
   }).isRequired,
 
 };
