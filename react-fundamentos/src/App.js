27c376a4b9132006989c430452c99f4479b9b9b7
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2022-06-10 21:28:45 -0300

    Trabalhando com States

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import Header from "./Header";
 
-const posts = [
-  { title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
-  { title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
-  { title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
-  { title: 'Title#04', subtitle: 'Sub#04', likes: 60 },
-];
-
 function App() {
+  const [posts, setPosts] = useState([
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10 },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50 },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 60 },
+  ]);
+
+  console.log({ posts });
+
+  function handleRefresh() {
+    setTimeout( () => {
+      setPosts((prevState) => [
+        ...prevState,
+        {
+          id: Math.random(),
+          title: `Title#0${prevState.length + 1}`,
+          subtitle: `Sub#0${prevState.length + 1}`,
+          likes: 50,
+        }
+      ]);
+    }, 2000);
+  }
+
   return (
     <>
       <Header >
         <h2>Posts da semana</h2>
+        <button onClick={handleRefresh}>Atualizar</button>
       </Header>
 
       <hr />
 
-      {posts.map( post => (
+      {posts.map(post => (
         <Post
-        key={post.title}
+          key={post.title}
           likes={post.likes}
           post={{
             title: post.title,
