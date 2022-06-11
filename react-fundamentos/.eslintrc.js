53c3f51e7382fc1a89b6ce6f74408a658a6c6e80
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2022-06-11 16:28:59 -0300

    Adicionando editorconfig e eslint no projeto

---
+++ react-fundamentos/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'class-methods-use-this': 'off',
+    'consistent-return': 'off',
+    'prefer-const': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/button-has-type': false,
+  },
+};
