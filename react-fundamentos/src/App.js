8d04f26a6ecfc58db5d1705ead77b830b069502b
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2022-06-15 21:55:22 -0300

    Classes dinâmicas

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -9,16 +9,16 @@ import styles from './App.scss';
 function App() {
   const [posts, setPosts] = useState([
     {
-      id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false,
+      id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false, removed: true,
     },
     {
-      id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true,
+      id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true, removed: false,
     },
     {
-      id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false,
+      id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false, removed: false,
     },
     {
-      id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 60, read: false,
+      id: Math.random(), title: 'Title#04', subtitle: 'Sub#04', likes: 60, read: false, removed: false,
     },
   ]);
 
@@ -35,8 +35,9 @@ function App() {
   }
 
   function handleRemovePost(postId) {
-    console.log({ postId });
-    setPosts((prevState) => prevState.filter((post) => post.id !== postId));
+    setPosts((prevState) => prevState.map(
+      (post) => (post.id === postId ? { ...post, removed: true } : post),
+    ));
   }
 
   return (
