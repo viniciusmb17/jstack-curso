00f0a4ae3b18ee97ce90d4912a76ec9ebd502375
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2022-06-18 18:15:39 -0300

    Desafios (parte 2) - Custom Hooks

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './styles/global';
@@ -7,7 +7,7 @@ import Layout from './components/Layout';
 import themes from './styles/themes';
 
 function App() {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useLocalState('dark');
 
   const currentTheme = useMemo(() => themes[theme] || themes.dark, [theme]);
 
@@ -23,3 +23,25 @@ function App() {
 }
 
 export default App;
+
+function useLocalState(key, initialValue = '') {
+  const [state, setState] = useState(() => {
+    const storedData = localStorage.getItem(key);
+
+    if (storedData) {
+      // Se houver algo salvo, retorna ...
+      return JSON.parse(storedData);
+    }
+
+    // Se não houver nada salvo, retorna initialValue ...
+    return initialValue;
+  });
+
+  useEffect(() => {
+    // Toda vez que state for alterado, salva no localStorage ...
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  // Deixamos o state e o setState acessíveis para quem for usar este hook...
+  return [state, setState];
+}
