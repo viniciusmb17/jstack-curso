c3f6560b196e9a1447d7ecbdf33975add0c6b3c6
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2022-07-28 18:42:24 -0300

    removing react-fundamentos folder

--- react-fundamentos/src/styles/global.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-
-export default createGlobalStyle`
-  body {
-    background: ${(props) => props.theme.currentTheme.backgroundColor};
-    color: ${(props) => props.theme.currentTheme.textColor};
-    font-family: sans-serif;
-  }
-`;
--- react-fundamentos/src/styles/themes/common.js
+++
@@ -1,8 +0,0 @@
-export default {
-  spacing: {
-    small: 8,
-    medium: 16,
-    large: 24,
-  },
-  borderRadius: '10px',
-};
